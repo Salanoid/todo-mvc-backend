55eb62ce15ebbc7fa9562eb657d00365fe88d080
Salanoid <salanoid@gmail.com>
2025-02-11 10:33:27 +0200

    fix rebase issues

--- app/graphql/types/todo_type.rb
+++ app/graphql/types/todo_type.rb
@@ -1,6 +1,5 @@
 module Types
   class TodoType < Types::BaseObject
-    field :body, String, null: true, description: "Content of todo"
     description "Todo Type"
 
     field :id, ID, null: false, description: "ID of the todo"
